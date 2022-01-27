6da8b90145d587ebaff0b0bdec72ef88b3b3d6dc
tusharsharma000 <84301919+tusharsharma000@users.noreply.github.com>
2022-01-27 17:26:28 +0530

    string

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import '../styles/App.css';
-import Welcome from './Welcome';
+import "../styles/App.css";
+import Welcome from "./Welcome";
 
 const App = () => {
-  return (
-    <Welcome name="" />
-  )
-}
-
+  let name = "Tushar";
+  return <Welcome name={name} />;
+};
 
 export default App;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from "react";
+
+const Welcome = ({ name }) => {
+  let sayHi = `Hey ${name}!`;
+  let greetMessage = `Welcome to Newton School.`;
+  return (
+    <div>
+      <h1>{sayHi}</h1>
+      <h2>{greetMessage}</h2>
+    </div>
+  );
+};
+export default Welcome;
